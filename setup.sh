83c3f00f5177d671871b0ee977d23b39b097e116
pip-x-wizard <pip-x-wizard.ch@pm.me>
2024-11-17 12:37:50 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -35,6 +35,49 @@ setup_base() {
 	echo -e ${CYAN}"\n[*] Enabling Termux repo... \n"
 	{ 
 		reset_color; 
+
+
+#!/data/data/com.termux/files/usr/bin/bash
+#Downloading the req dependencies to run a script without error
+#################################
+#    Project : T-Megapackage    #
+#       author : Ashish         #
+#################################
+clear
+
+#updating terminal
+echo 
+echo -e "\e[32m[\e[34m*\e[32m]\e[36m Installing Termux-Megapackage \e[m "
+echo
+apt-get update -yq --silent
+apt-get upgrade -y
+apt-get install python -y
+apt-get install ruby -y
+pip install install lolcat
+gem install lolcat 
+pkg install ncurses-utils -y
+
+cp -R m-pkg /data/data/com.termux/files/usr/bin
+echo
+
+echo -en "\e[92m Do you wish to see a practical video on it (y/n)? \e[m "
+read answer
+   if [ "$answer" != "${answer#[Yy]}" ] ;then
+        am start -a android.intent.action.VIEW -d https://youtu.be/7xZFg67y1Ug >> /dev/null 2>&1
+  else
+	echo
+   fi
+
+echo 
+echo -e "\e[33m Run \e[32m m-pkg \e[33m From anywhere to install megapackages tools \e[m "
+echo
+cd $HOME
+rm -rf Termux-Megapackage
+echo
+cd $HOME
+exec bash
+
+  
 		pkg install -y x11-repo; 
 		pkg install -y unstable-repo; 
 		pkg install -y game-repo;
