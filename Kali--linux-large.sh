51d15d2c9210dabfaabfb92fdef8cd02c08eee2e
pip-x-wizard <pip-x-wizard.ch@pm.me>
2024-11-17 12:23:58 -0700

    Update Kali--linux-large.sh

--- Kali--linux-large.sh
+++ Kali--linux-large.sh
@@ -9,13 +9,13 @@ apt update && apt install wget -y
 wget -O install_kali-linux-large
 https://github.com/pip-x-wizard/install-kali-termux.
 git/install-kali-termux/
-master/install[~]
-chmod +x ./kali-linux-large [~]
+master/install
+chmod +x ./kali-linux-large 
 sudo apt-get full-upgrade -y
 sudo apt-get dist-upgrade 
 sudo apt-get install 
 --only-upgrade package_
-kali-linux-large [~]
+kali-linux-large 
 
 
 ###############################
