bf2806354eb0887aef6ba9047d0efd6b6761fd98
pip-x-wizard <pip-x-wizard.ch@pm.me>
2024-11-17 10:41:21 -0700

    Update and rename Kali-install.sh to Kali--linux-large.sh
    #############################
    
    pkg install wget
    apt update && apt install wget -y
    wget -O install_kali-linux-large
    https://github.com/pip-x-wizard/install-kali-termux.
    git/install-kali-termux/
    master/install.sh
    chmod +x ./kali-linux-large.sh
    
    #############################

---
+++ Kali--linux-large.sh
@@ -0,0 +1,12 @@
+
+###############################
+
+pkg install wget
+apt update && apt install wget -y
+wget -O install_kali-linux-large
+https://github.com/pip-x-wizard/install-kali-termux.
+git/install-kali-termux/
+master/install.sh
+chmod +x ./kali-linux-large.sh
+
+###############################
